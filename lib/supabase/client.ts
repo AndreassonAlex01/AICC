746d315f4ef8f46ac3685f67658a29d76c2e1993
AndreassonAlex01 <andreassonalex01@gmail.com>
2026-07-18 13:21:02 +0200

    removed /rest/v1/ in url

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -3,7 +3,7 @@ import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL = "https://qhaaxqckkupsjjvopvrf.supabase.co/rest/v1/",
+    process.env.NEXT_PUBLIC_SUPABASE_URL = "https://qhaaxqckkupsjjvopvrf.supabase.co",
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY= "sb_publishable_-7FQBhppNoOsc2IIS0I1IA_Wk1Li9s3"
   );
 }
--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -5,7 +5,7 @@ import { cookies } from "next/headers";
 export async function createClient() {
   const cookieStore = await cookies();
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL = "https://qhaaxqckkupsjjvopvrf.supabase.co/rest/v1/",
+    process.env.NEXT_PUBLIC_SUPABASE_URL = "https://qhaaxqckkupsjjvopvrf.supabase.co",
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY = "sb_publishable_-7FQBhppNoOsc2IIS0I1IA_Wk1Li9s3",
     {
       cookies: {
